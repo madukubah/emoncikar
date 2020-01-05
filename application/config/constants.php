2292456cfcc33089aaf4279f5e2bd4856d183048
alanHetfielD <muhalfalah1998@gmail.com.com>
2020-01-05 16:35:54 +0800

    aa

--- application/config/constants.php
+++ application/config/constants.php
@@ -85,7 +85,7 @@ defined('EXIT__AUTO_MIN')      or define('EXIT__AUTO_MIN', 9); // lowest automat
 defined('EXIT__AUTO_MAX')      or define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
 // custom
-define('APP_NAME', 'Emon Cikar');
+define('APP_NAME', 'eMON CIKAR');
 define('APP_AUTHOR', 'FIXL Labs');
 define('APP_DESC', 'FIXL base core for creating Information System base on Codeigniter');
 define('FAVICON_IMAGE', 'assets/logo/favicon.ico');
