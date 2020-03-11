1440c60d1a969ee00e841369f198b9cf134bc64c
madukubah <muhalfalah1998@gmail.com>
2020-03-12 00:10:23 +0800

    tes

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://ecikarsultra.com/';
-$config['base_url'] = 'http://localhost/emoncikar/';
+$config['base_url'] = 'https://ecikarsultra.com/';
+// $config['base_url'] = 'http://localhost/emoncikar/';
 
 /*
 |--------------------------------------------------------------------------
